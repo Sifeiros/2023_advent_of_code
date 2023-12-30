58c3dbdcc264bb35b85447ae4216b45a5a4e21c7
Lukas Küpper <lukas.kuepper@gmail.com>
2023-12-31 00:58:45 +0100

    Finished Day51

--- src/day_05/day_051.py
+++ src/day_05/day_051.py
@@ -3,17 +3,87 @@ from pathlib import Path
 import numpy as np
 import pathlib
 
+from typing import List
+
+class CombinedTranslator:
+    parts: List["PartialTranslator"]
+    domain_number: int
+    def __init__(self, num: int):
+        self.parts = list()
+        self.domain_number = num
+
+    def add_part_translator(self, prt_trans: "PartialTranslator"):
+        self.parts.append(prt_trans)
+
+    def convert(self, src_num):
+        tar_num = -1
+        for part in self.parts:
+            if part.is_in_domain(src_num) and tar_num == -1:
+                tar_num = part.convert_to_target(src_num)
+            elif part.is_in_domain(src_num) and tar_num != -1:
+                raise ValueError("Duplicate Domain of Definition.")
+        if tar_num == -1:
+            tar_num = src_num
+        return tar_num
+
+class PartialTranslator:
+    source_start: int #inclusive
+    source_end: int #exclusive
+    target_start: int #inclusive
+    target_end: int #exclusive
+    length: int
+
+    def __init__(self, trg_st: int, src_st: int, leng: int):
+        self.source_start = src_st
+        self.source_end = src_st+leng
+        self.target_start = trg_st
+        self.target_end = trg_st + leng
+        self.length = leng
+
+    def is_in_domain(self, src_num: int):
+        return self.source_start <= src_num < self.source_end
+
+    def convert_to_target(self, src_num: int):
+        if self.is_in_domain(src_num):
+            return src_num - self.source_start + self.target_start
+        else:
+            raise ValueError
 
 
 def plant_seeds(path):
     raw_input = read_input_file(path)
 
-    sum = 0
+    dom_num = 0
+    translator = {}
+    seeds: str
+    for line in raw_input:
+        if line.strip() == "":
+            dom_num += 1
+            translator[dom_num] = CombinedTranslator(dom_num)
+        elif "map" in line:
+            continue
+        elif dom_num > 0:
+            tmp_numbers = [int(num) for num in line.strip().split(" ")]
+            tmp_trans = PartialTranslator(tmp_numbers[0], tmp_numbers[1], tmp_numbers[2])
+            translator[dom_num].add_part_translator(tmp_trans)
+        elif dom_num == 0:
+            seeds = line.split(":")[1].strip().split(" ")
 
-    return sum
+    loc_number = 999999999999
+    tmp_print_str = ""
+    for seed in seeds:
+        src_num = int(seed)
+        tmp_print_str = ""+seed
+        for i in range(1,dom_num+1):
+            src_num = translator[i].convert(src_num)
+            tmp_print_str += "," + str(src_num)
+        if src_num < loc_number:
+            loc_number = src_num
+        print(tmp_print_str)
+    return loc_number
 
 
 if __name__ == "__main__":
-    path = Path("./data/day_03/data_03.txt")
+    path = Path("./data/day_05/data_05.txt")
     result = plant_seeds(path)
     print(result)
--- test/day_05/day_051.py
+++ test/day_05/day_051.py
@@ -2,12 +2,40 @@ import unittest
 from pathlib import Path
 from src.day_05 import day_051
 
-class Day03_Test(unittest.TestCase):
-    def test_sum_of_parts(self):
-        test_input_path = Path("./data/day_03/test_031.txt")
+class Day05_Test(unittest.TestCase):
+    def test_seed_location(self):
+        test_input_path = Path("./data/day_05/test_051.txt")
 
         result = day_051.plant_seeds(test_input_path)
         self.assertEqual(35, result)  # add assertion here
+
+    def test_partial_translator(self):
+        test_conv = day_051.PartialTranslator(50, 98, 2)
+
+        self.assertEqual(98, test_conv.convert_to_target(50))
+        self.assertEqual(99, test_conv.convert_to_target(51))
+        with self.assertRaises(ValueError):
+            test_conv.convert_to_target(52)
+
+    def test_combined_translator(self):
+        test_conv_1 = day_051.PartialTranslator(50, 98, 2)
+        test_conv_2 = day_051.PartialTranslator(52, 50, 48)
+
+        test_comb_conv = day_051.CombinedTranslator(1)
+        test_comb_conv.add_part_translator(test_conv_1)
+        test_comb_conv.add_part_translator(test_conv_2)
+
+        self.assertEqual(98, test_comb_conv.convert(50))
+        self.assertEqual(99, test_comb_conv.convert(51))
+        self.assertEqual(50, test_comb_conv.convert(52))
+        self.assertEqual(60, test_comb_conv.convert(62))
+        self.assertEqual(97, test_comb_conv.convert(99))
+        self.assertEqual(100, test_comb_conv.convert(100))
+        self.assertEqual(49, test_comb_conv.convert(49))
+
+
+
+
 
 
 if __name__ == '__main__':
